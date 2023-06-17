cb7a5b1c08f08ed51b040eb317559587a2f7f05a
nihility1214 <nihility1214@163.com>
2023-06-17 16:00:40 +0800

    1

--- src/main/java/com/example/jpetstore/Common/MyConfigurer.java
+++ src/main/java/com/example/jpetstore/Common/MyConfigurer.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyConfigurer implements WebMvcConfigurer {
     private static final String staticResourcePath = "/image/look/**";
 
-    private static final String mapToLocalPath = "file:/home/raymond/PetImage/";    // TODO 配置本地图片文件夹
+    private static final String mapToLocalPath = "file:/home/wzf/PetImage/";    // TODO 配置本地图片文件夹
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
